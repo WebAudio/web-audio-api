d7b466125ee27ffafe0d356a7650842756026eaa
Raymond Toy <rtoy@chromium.org>
2018-07-13 10:18:55 -0700

    Really do everything.
    Logic for doing everything was backwards.

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ set -x
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 #if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-if true; then
+if false; then
     echo "Skipping deploy; just doing a build."
     doCompile
     exit 0

cadcf6b6e3d4cdcb98517a7694c77da855ddf480
Raymond Toy <rtoy@chromium.org>
2018-08-09 14:29:02 -0700

    Keep BSLOG
    Don't remove BSLOG when running compile.sh.  This is useful when
    running compile.sh locally so you can update the expected errors from
    BSLOG if needed.

--- compile.sh
+++ compile.sh
@@ -12,6 +12,8 @@ ERRLOG="actual-errs.txt"
 # can update the expected errors.
 trap "rm $ERRLOG" 0
 
+echo $PWD
+ls
 # Run bikeshed and save the output.  You can use this output as is
 # to update expected-errs.txt.
 bikeshed --print=plain -f spec 2>&1 | tee $BSLOG

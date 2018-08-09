8c4748125df3a6400876992cc170a7c6aae6987e
Raymond Toy <toy.raymond@gmail.com>
2018-08-09 14:40:17 -0700

    Rebase with upstream, fixing differences.

--- compile.sh
+++ compile.sh
@@ -12,11 +12,6 @@ ERRLOG="actual-errs.txt"
 # can update the expected errors.
 trap "rm $ERRLOG" 0
 
-echo $PWD
-ls
-git co $TRAVIS_BRANCH
->>>>>>> Keep BSLOG
-
 # Run bikeshed and save the output.  You can use this output as is
 # to update expected-errs.txt.
 bikeshed --print=plain -f spec 2>&1 | tee $BSLOG

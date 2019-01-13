7fd2ab35e4d08c80264159a22b645972117de625
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-13 17:23:04 +0000

    Deploy WebAudio/web-audio-api to github.com/WebAudio/web-audio-api.git:gh-pages

--- bikeshed/bikeshed/metadata.py
+++ bikeshed/bikeshed/metadata.py
@@ -613,14 +613,14 @@ def parseRepository(key, val, lineNum):
         return Repository(url=pieces[0], name=pieces[1])
     elif len(pieces) == 1:
         # Try to recognize a GitHub url
-        match = re.match("https://github.com/([\w-]+)/([\w-]+)/?$", val)
+        match = re.match("https://github\.([\w.-]+)/([\w-]+)/([\w-]+)/?$", val)
         if match:
             return GithubRepository(*match.groups())
-        # If you just provide a user/repo pair, assume it's a github repo.
+        # If you just provide a user/repo pair, assume it's a github.com repo.
         # Will provide ways to opt into other repos when people care.
         match = re.match("([\w-]+)/([\w-]+)$", val)
         if match:
-            return GithubRepository(*match.groups())
+            return GithubRepository("com", *match.groups())
         # Otherwise just use the url as the shortname
         return Repository(url=val)
     else:
@@ -831,10 +831,10 @@ def getSpecRepository(doc):
             with open(os.devnull, "wb") as fnull:
                 remotes = subprocess.check_output(["git", "remote", "-v"], stderr=fnull)
             os.chdir(old_dir)
-            search = re.search(r"origin\tgit@github\.com:([\w-]+)/([\w-]+)\.git \(\w+\)", remotes)
+            search = re.search(r"origin\tgit@github\.([\w.-]+):([\w-]+)/([\w-]+)\.git \(\w+\)", remotes)
             if search:
                 return GithubRepository(*search.groups())
-            search = re.search(r"origin\thttps://github.com/([\w-]+)/([\w-]+)\.git \(\w+\)", remotes)
+            search = re.search(r"origin\thttps://github\.([\w.-]+)/([\w-]+)/([\w-]+)\.git \(\w+\)", remotes)
             if search:
                 return GithubRepository(*search.groups())
             return config.Nil()

730614ac494a073213e09c394b8b739da5950e07
Paul Adenot <paul@paul.cx>
2019-02-19 16:43:34 +0100

    fix the build by not using tee

--- compile.sh
+++ compile.sh
@@ -14,12 +14,20 @@ trap "rm $ERRLOG" 0
 
 # Run bikeshed and save the output.  You can use this output as is
 # to update expected-errs.txt.
-bikeshed --print=plain -f spec 2>&1 | tee $BSLOG
+bikeshed --print=plain -f spec 2>&1 > $BSLOG
 # Remove the line numbers from the log, and make sure it ends with a
 # newline.
 sed 's;^LINE [0-9]*:;LINE:;' $BSLOG | sed -e '$a\' > $ERRLOG
 # Do the same for the expected errors and compare the two.  Any
 # differences need to be fixed.  Exit with a non-zero exit code if
 # there are any differences.
-(sed 's;^LINE [0-9]*:;LINE:;' expected-errs.txt | sed -e '$a\' | diff -u - $ERRLOG) || exit 1
+
+sed 's;^LINE [0-9]*:;LINE:;' expected-errs.txt | sed -e '$a\' | diff -u - $ERRLOG
+
+if [[ $? -ne 0 ]]
+then
+  echo "Actual errors are not exactly what is expected, log provided below:"
+  cat $BSLOG
+  exit 1
+fi
 
